a2ad6d254331d7773950a973d57126fc69478111
GurumNyang <gurumnyang@gmail.com>
2025-03-24 14:16:53 +0900

    Update index.js
    CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 4715;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
